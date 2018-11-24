a3d90993c68bb968682fc3723333d6ba07899c3b
wangmin <609566545@qq.com>
2018-11-24 22:35:24 +0800

    吧本地工程添加到git

---
+++ src/com/itheima/first/FirstClass.java
@@ -0,0 +1,11 @@
+package com.itheima.first;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
